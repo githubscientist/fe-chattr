dd0b67972c0ff54c0dd6d670203b058644ab8fc9
githubscientist <sathish.datascientist@gmail.com>
2025-03-01 12:27:54 +0530

    login page flow done

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,45 @@
-import { Link } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router";
+import { selectEmail, selectPassword, setEmail, setPassword } from "../redux/features/auth/loginSlice";
+import authServices from "../services/authServices";
+import { toast } from "react-toastify";
 
 const LoginPage = () => {
+
+    const email = useSelector(selectEmail);
+    const password = useSelector(selectPassword);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        authServices.login({ email, password })
+            .then((response) => {
+                toast.success(response.data.message);
+
+                // clear the form
+                dispatch(setEmail(''));
+                dispatch(setPassword(''));
+
+                // navigate the user to the dashboard page
+                setTimeout(() => {
+                    navigate('/dashboard');
+                }, 500);
+            })
+            .catch((error) => {
+                toast.error(error.response.data.message);
+            });
+    }
+
     return (
         <div className="container mt-5 text-center">
             <h1 className="text-4xl text-gray-800">Login to Chattr!</h1>
             <h4 className="text-xl text-gray-600">A simple chat application</h4>
 
             <div className="max-w-2xl mx-auto mt-5 bg-white shadow-md p-5 rounded-lg overflow-hidden border border-gray-200 px-5 py-5">
-                <form>
+                <form onSubmit={handleLogin}>
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
                             Email
@@ -17,6 +49,8 @@ const LoginPage = () => {
                             id="username"
                             type="text"
                             placeholder="Email"
+                            value={email}
+                            onChange={(e) => dispatch(setEmail(e.target.value))}
                         />
                     </div>
                     <div className="mb-6">
@@ -28,12 +62,14 @@ const LoginPage = () => {
                             id="password"
                             type="password"
                             placeholder="*********"
+                            value={password}
+                            onChange={(e) => dispatch(setPassword(e.target.value))}
                         />
                     </div>
                     <div className="flex items-center justify-between">
                         <button
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                            type="button"
+                            type="submit"
                         >
                             Login
                         </button>
